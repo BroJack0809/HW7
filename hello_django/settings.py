763c1e31269f17f7e36247d59be578dc96629f1f
BroJack0809 <79563574+BroJack0809@users.noreply.github.com>
2021-05-31 22:40:14 +0800

    second commit

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7c7samk$t8lsbsn_0+av2m(^a7j7dy%39&qm49g@(#w*trv4b&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://dashboard.heroku.com/apps/brojack0809','127.0.0.1']
 
 
 # Application definition
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
