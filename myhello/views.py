faa0e23bd6bbbc1e15dc0e17db58a3fd0de55c6c
BroJack0809 <79563574+BroJack0809@users.noreply.github.com>
2021-05-31 23:27:57 +0800

    six

--- myhello/views.py
+++ myhello/views.py
@@ -1,7 +1,7 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
 def myIndex(request):
-    my_name=request.POST.get('name',"CGU")
+    my_name=request.POST.get('name',"Heroku!!!")
     return HttpResponse("Hello"+my_name)
