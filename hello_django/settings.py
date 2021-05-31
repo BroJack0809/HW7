a66c2b32816f945b4cd330b081de98d62f18f63b
BroJack0809 <79563574+BroJack0809@users.noreply.github.com>
2021-06-01 00:38:52 +0800

    eleven

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9%fmk!*^c4uht7_wwli%8kqsre&31!bm+@5d=w5m-f0ll0qz0n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://hw7b0829006.herokuapp.com','127.0.0.1']
+# ALLOWED_HOSTS = ['https://hw7b0829006.herokuapp.com','127.0.0.1']
 
 
 # Application definition
